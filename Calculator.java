71d93caf4f17a7c332585643ad74c83943eb482e
Shavunen <snhavunen@quinnipiac.edu>
2022-02-15 19:12:28 -0500

    asked user to enter two numbers and add them together

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 
 public class Calculator {
 
 	public static void main(String[] args) {
-		scanner SC = mew Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Please enter the first number");
+		int x = scanner.nextInt();
+		
+		System.out.println("Please enter the second number");
+		int y=scanner.nextInt();
+		
+		int sum = x + y; 
+		
+			
+		 System.out.println("This is the sun of your inputs "+ sum); 
+					
 		
 
 	}
